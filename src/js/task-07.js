dc33c135bb0674be2abf5ee598546f61b746006f
DmViktoriya <dmytrenkoviktoriya@gmail.com>
2021-07-18 19:43:42 +0300

    Added task 07

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,9 +1,16 @@
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
-function changeFontSize((target: { value })) {
-        spanRef.style.fontSize = value +"px"
-}
+function changeFontSize(_target: {value}) {
+    spanRef.style.fontSize = value + "px";
+};
 
-//inputRef.oninput = changeFontSize;
+//console.log(changeFontSize);
+
+
+//function changeFontSize(target: value) {
+//    spanRef.style.fontSize = value + "px";
+//};
+
+inputRef.oninput = changeFontSize;
 
