2fb9641409678fd768c2e4493487dfc4457d2eee
DmViktoriya <dmytrenkoviktoriya@gmail.com>
2021-07-24 13:56:22 +0300

    Fexed tasks 3 and 5

--- js/task-07.js
+++ js/task-07.js
@@ -14,3 +14,13 @@ function changeFontSize({ target: { value } }) {
 
 inputRef.oninput = changeFontSize;
 
+
+
+// Задание 7
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// <input id="font-size-control" type="range" />
+// <br />
+// <span id="text">Абракадабра!</span>
+
+
+
